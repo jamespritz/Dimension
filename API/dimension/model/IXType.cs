f9f657f90fad062efaa2ed4fcd93cb384816d8a5
James <james@jamespritz.com>
2013-11-19 07:23:38 -0500

    create model definitoins for dimension

---
+++ API/dimension/model/IXType.cs
@@ -0,0 +1,11 @@
+﻿using intrinsic.xl8.model.client;
+
+namespace intrinsic.dimension.model {
+    public interface IXType {
+        int ID { get; }
+
+        IResource Literal { get; }
+        IResource Description { get; }
+
+    }
+}
